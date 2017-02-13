4fa386b27ec5410066b034a55557631fa629cf7d
dawa <dawa@HZLL0DAWA1>
2017-02-13 11:54:03 +0000

    Add project files.

---
+++ src/CoreBlogger/Models/Blog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlogger.Models
+{
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+
+        public string Owner { get; set;}
+
+        public virtual List<Post> Posts { get; set; }
+
+    }
+}
